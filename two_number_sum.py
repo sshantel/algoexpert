f2ef3ae570aee7530abae5fd270a28ce0b31774b
Chantel Yip <chantelyip422@gmail.com>
2021-04-16 01:06:25 -0700

    added testcases

--- two_number_sum.py
+++ two_number_sum.py
@@ -1,6 +1,20 @@
-def twoNumberSum(array, targetSum):
-	for i in range(len(array)):
-		for j in range(i+1, len(array)):
-			if array[i] + array[j] == targetSum:
-				return array[i], array[j]
-	return []
+import unittest
+
+class TestProgram(unittest.TestCase):
+    def test_case_1(self):
+        output = twoNumberSum([3, 5, -4, 8, 11, 1, -1, 6], 10) 
+        self.assertTrue(len(output) == 2)
+        self.assertTrue(11 in output)
+        self.assertTrue(-1 in output)
+
+
+def twoNumberSum(array, targetSum): 
+    for i in range(len(array)):
+        for j in range(i+1, len(array)):
+            if array[i] + array[j] == targetSum:
+                return array[i], array[j]
+    return []
+
+
+if __name__ == '__main__':
+    unittest.main()
