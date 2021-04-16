ed9fb065ccc437de3d3d80964f6373f27f83b59a
Chantel Yip <chantelyip422@gmail.com>
2021-04-16 01:19:25 -0700

    added unittests and passed

--- validate_subsequence.py
+++ validate_subsequence.py
@@ -1,5 +1,13 @@
+import unittest
+class TestProgram(unittest.TestCase):
+    def test_case_1(self):
+        array = [5, 1, 22, 25, 6, -1, 8, 10]
+        sequence = [1, 6, -1, 10]
+        self.assertTrue(isValidSubsequence(array, sequence))
+
+
 def isValidSubsequence(array, sequence):
-    # create two pointers
+# create two pointers
     seq = 0
     for value in array:
         if seq == len(sequence):
@@ -9,3 +17,7 @@ def isValidSubsequence(array, sequence):
 
     return seq == len(sequence)
 
+
+
+if __name__ == '__main__':
+    unittest.main()
