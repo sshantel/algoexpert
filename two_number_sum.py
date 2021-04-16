a323914630f10e715df07b5e70a7b9927a6c5d0b
Chantel Yip <chantelyip422@gmail.com>
2021-04-16 00:55:38 -0700

    passed all 12 test cases

--- two_number_sum.py
+++ two_number_sum.py
@@ -1,10 +1,6 @@
 def twoNumberSum(array, targetSum):
-    for i in range(len(array)):
-        first = array[i]
-        for j in range(i + 1, len(array)):
-            print(j)
-            second = array[j]
-            if first + second == targetSum:
-                return [first, second]
-    return []
-
+	for i in range(len(array)):
+		for j in range(i+1, len(array)):
+			if array[i] + array[j] == targetSum:
+				return array[i], array[j]
+	return []
