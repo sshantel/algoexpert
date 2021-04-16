9defb6621b7331d4e722bfab5cbe0a2f69d3a398
Chantel Yip <chantelyip422@gmail.com>
2021-04-16 10:31:12 -0700

    passed 10 test cases out of 17

--- sorted_squared_array.py
+++ sorted_squared_array.py
@@ -3,3 +3,8 @@ Write a function that takes in a non-empty array of integers that are sorted in 
 and returns a new array of the same length with the squares of the original integers
 also sorted in ascending order.
 """
+
+def sortedSquaredArray(array):
+    for i in range(len(array)):
+        array[i] = pow(array[i], 2)
+    return array
